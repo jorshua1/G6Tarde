3ffbfae61ee088581af34acd520e905668e6e759
“WilmerMN” <wilmermn369@gmail.com>
2023-02-28 14:22:39 -0500

    se agregado el numero 5

--- JavaScript/Hola.js
+++ JavaScript/Hola.js
@@ -1,1 +1,2 @@
 console.log("Hola mundo"); 
+console.log("Prueba numbre 25")
---
+++ JavaScript/Variable.js
@@ -0,0 +1,29 @@
+var vName = "Wilmer";
+console.log(vName);
+console.log(typeof(vName));
+
+
+var vPi = 3.141592;
+console.log(vPi);
+console.log(typeof(vPi));
+
+var vNumberIntiger = 29;
+console.log(vNumberIntiger);
+console.log(typeof(vNumberIntiger));
+
+var vTrue = true
+var vFalse = false
+console.log(vTrue, vFalse);
+console.log(typeof(vTrue, vFalse));
+
+var vNull = null
+console.log(vNull);
+console.log(typeof(vNull));
+
+/*var vOldYear = 21;*/
+var vPerson = { vName , vOldYear: 21 };
+console.log(vPerson);
+console.log(typeof(vPerson))
+
+
+
