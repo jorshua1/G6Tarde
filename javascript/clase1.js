2e9a3c61d131de8a9ad054b5ba40e9ff2f725b13
brayansantamaria <brayan.santamaria.2002@gmail.com>
2023-02-28 17:22:09 -0500

    clase viernes

---
+++ javascript/clase1.js
@@ -0,0 +1,24 @@
+let uno=1;
+let dos=2;
+let cadena="esto es un string";
+let entero= 155;
+var hola= "hola";
+let multiplicar=uno*dos;
+console.log(multiplicar);
+let h= "hola";
+let m= "mundo";
+let hm= h + m;
+console.log(hm);
+let decimal=152.54;
+console.log(typeof(decimal));
+let verdad = true;
+let indefinido;
+let objeto={nombre:"brayan", cedula:1001000111, acceso:true};
+let arreglo =["hola ","tengo ","hambre " ,"que","va","a","gastar"];//la posicion de los arreglos empieza desde 0
+let mensaje1=arreglo[0]+=arreglo[1]+=arreglo[2];
+console.log(mensaje1);
+
+
+
+
+
---
+++ javascript/variables.js
@@ -0,0 +1,13 @@
+var nombre = "brayan";
+var pi = 3.141592;
+var numeroentero = 5;
+let edad = 20; //let 
+console.log(edad);
+console.log(typeof(edad));
+console.log(numeroentero);
+console.log(typeof(numeroentero));
+console.log(pi);
+console.log(typeof(pi));
+console.log(nombre);
+
+
