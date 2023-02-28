3f72a3a020c630cf2d8711117471afc700c9d6e3
Nicolas Ruiz <nicosandr22@gmail.com>
2023-02-28 17:20:27 -0500

    commit variavles

---
+++ javaScript/variables.js
@@ -0,0 +1,38 @@
+var nombre = "Nicolas Ruiz";
+console.log(nombre);
+console.log(typeof(nombre));
+var pi = 3.141592;
+console.log(pi);
+console.log(typeof(pi));
+
+const edad = 20;
+//edad =19;
+
+let indefinido 
+console.log(indefinido)
+
+//Comentario de una linea 
+
+/*comentario 
+de varias 
+lineas */
+
+let cadena = "Esto es un string";
+let entero = 155;
+let decimal = 123.213;
+let verdad = true;
+let nulo = null;
+let objeto = {nombre: "Nicolas ", cedula: 1005, acceso: true};
+
+console.log(objeto.nombre)
+console.log(objeto["cedula"])
+
+let arreglo = [1, "hola", {nombre: "Nicolas"}, false, null]
+arreglo[1]= "reasignacion"
+console.log(arreglo[1]) //muestra la posicion 1 de arreglo
+
+objeto.nombre ="santiago"
+console.log(objeto.nombre)
+
+console.log (1 / 2) // 0.5
+console.log ( 1.0 / 2.0) // Esto es true
