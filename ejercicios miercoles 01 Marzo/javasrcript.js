993c6055a7b23f42f37d02b3584097dc033b9f8b
sergioArdila <sergiochecho15@hotmail.com>
2023-03-01 15:50:43 -0500

    segundo ejercicio de velocidad

--- ejercicios miercoles 01 Marzo/javasrcript.js
+++ ejercicios miercoles 01 Marzo/javasrcript.js
@@ -1,13 +1,17 @@
-
+/*
 var num1= parseInt(prompt("ingrese el primer numero"));
 var num2= parseInt(prompt("ingrese el segundo numero"));
 var resultado = num1+num2;
 alert("el resultado es= " + resultado);
+*/
 
 
 
-
-
+let velocar1 = parseInt(prompt("ingrese la velocidad del primer vehiculo en kilometros"));
+let velocar2 = parseInt(prompt("ingrese la velocidad del segundo vehiculo en kilometros"));
+let distancia= parseInt(prompt("ingrese la distancia en la que estan los vehiculos"));
+let tiempo= (distancia/velocar2-velocar1)*60;
+alert("El tiempo en que se encuentran es: " + tiempo + "Minutos")
 
 
 
