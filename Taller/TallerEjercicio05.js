75d834c7ff89c3437d9da8fd2291a811bc8798a1
“WilmerMN” <wilmermn369@gmail.com>
2023-03-01 17:59:10 -0500

    Se agrego el punto 5

---
+++ Taller/TallerEjercicio05.js
@@ -0,0 +1,12 @@
+//-------------------------EJERCICIO #5---------------------------
+
+{
+    let vPromedio;
+    let vNota01 = parseFloat(prompt("Ingrese la primera nota"));
+    let vNota02 = parseFloat(prompt("Ingrese la segunda nota"));
+    let vNota03 = parseFloat(prompt("Ingrese la tercera nota"));
+    let vNota04 = parseFloat(prompt("Ingrese la cuarta nota"));
+    let vPorcentajeImportante = 0.6;
+    let vPorcentajeRegular = 0.4;
+}
+    
