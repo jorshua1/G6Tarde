68dd5a3c1271ca8e4273819a8bd82cbd933216bf
CamilaDiaz <camiladiaz2708@gmail.com>
2023-03-01 15:01:42 -0500

    variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,37 @@
+var nombre = "Camila"
+console.log(nombre);
+console.log(typeof (nombre));
+var pi = 3.141592
+console.log(pi);
+console.log(typeof (pi));
+var aprobado = true
+console.log(aprobado);
+console.log(typeof (aprobado));
+var valornulo = "null"
+console.log(valornulo);
+console.log(typeof (valornulo));
+var colores = "rojo"
+console.log(colores);
+console.log(typeof (colores));
+let dog ={nombre: "Killer", race:"dogoArgentino"}
+console.log(dog);
+console.log(typeof (dog));
+let array = [1,2,3,4]
+const edad = "21"
+//edad = "hola"
+let indefinitely
+console.log(indefinitely);
+let object ={nombre:"camila",Cedula: 1005289147, edad:21};
+
+let array = [1. "hola", {clave: object.nombre},false];
+console.log()
+
+
+
+
+// hoy aprendimos como se desarrolla una variable
+/*ayer
+aprendimos
+a 
+que es una 
+variable*/
