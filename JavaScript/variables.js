f93d864ae4ea0fc7fcb9837f400fef46316089e1
Jonathan <jcarrillo366@misena.edu.co>
2023-03-01 15:03:32 -0500

    variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,69 @@
+let cadena = "esto es un string";
+let entero = 155;
+let decimal = 152.34;
+console.log(typeof decimal);
+let verdad = true;
+let nulo = null;
+let indefinido = undefined;
+let objeto = {nombre :"jonathan",cedula :1102379663,acceso :true};
+console.log(objeto.nombre);
+let arreglo = [1,"hola",{nombre: objeto.nombre}, false, null];
+arreglo[1] = "rivality"
+console.log(arreglo[1]);
+objeto.nombre = "tatan"
+console.log(objeto.nombre);
+// OPERADORES ARITMATICOS
+// SUMA
+let num1 = 3;
+let num2 = 4;
+let suma = num1 + num2;
+console.log(suma);
+//RESTA
+let resta = num1 - num2;
+console.log(resta);
+// DIVISION
+let division = num1 / num2;
+console.log(division);
+//MULTIPLICACION
+let multiplicacion = num1 * num2;
+console.log(multiplicacion);
+let modelado = num1 % num2;
+console.log(modelado);
+//exponencial
+let exp = num1**num2;
+console.log(exp);
+// DECREMENTO
+console.log(--num1);
+// COMPARACION
+let comparacion = num1==num2;
+console.log(comparacion);
+console.log(num1!=num2);
+console.log(num1<=num2);
+console.log(++num1);
+console.log(num1<num2);
+console.log(num1 === num2);
+//LOGICOS
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(true || false);
+console.log(false || false);
+console.log(num1<num2||false);
+console.log(!true);
+console.log(!false);
+x = 10;
+x = x/5;
+console.log(x);
+//ASIGNACION
+console.log(x*=3);
+console.log(x/=4);
+console.log(x-=5);
+console.log(x**=2);
+console.log(x+=5);
+console.log(x%=num2);
+var NumeroPorTeclado = parseInt(prompt("ingrese un numero"));
+
+
+
+
+
