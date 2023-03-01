fc5d03e93467d55ab77fb9af66497404937fe1f8
CarlosGonzalez <carlosgonzalezleal0@gmail.com>
2023-03-01 16:36:25 -0500

    se ad arch ejemplos1 y taller1

---
+++ javascrip/Variable.js
@@ -0,0 +1,24 @@
+/*var Nombre = "carlos";
+console.log(Nombre);
+console.log(typeof(Nombre));
+var pi = 3.1416;
+console.log(pi);
+console.log(typeof(pi));
+var int = 4;
+console.log(int);
+console.log(typeof(int));
+console.log(isImteger(int));*/
+
+/*var Verdadero=!!(1);
+console.log(typeof(Verdadero));*/
+
+
+var nn = 4
+var mm = 5
+if (nn < mm) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+}
