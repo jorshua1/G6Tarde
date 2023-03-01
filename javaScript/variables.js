543c26212ae9937dbaba553223ec7bf3d32db74f
juanEspinosa <tgajuan@hotmail.com>
2023-03-01 15:07:36 -0500

    lo que hice ayer uwu

---
+++ javaScript/variables.js
@@ -0,0 +1,38 @@
+var nombre = "Juan"
+console.log(nombre)
+/**jejejejeejjejejejejejej */
+const edad = 22
+//edad = "15"
+let indefinido
+console.log(indefinido)
+var num1 = 2;
+var num2 = 52;
+var resultado = num1*num2;
+console.log(resultado);
+let cadena = "esto es un string";
+let entero = 155;
+let decimal = 152.54;
+ console.log(decimal);
+let verdad = true; 
+let nulo = null;
+let objeto = {nombre:"JUAN",
+ Clave: "1097491641",
+ acceso: true}
+  console.log(objeto["nombre"])
+   let arreglo = [1, "hola", {nombre: "JUAN"}, false, null]
+   arreglo[1] = "usted me cae mal"
+console.log(arreglo[1])
+objeto.nombre="JUAN"
+console.log(objeto.nombre)
+let num3=25
+let num4=50
+
+let resultad = num3/num4
+console.log(resultad)
+let var1 = 20
+let var2 = 56
+let igual = var1>=var2
+console.log(igual)
+let var3 = 200;
+
+
