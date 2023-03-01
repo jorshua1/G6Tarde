2f7c70640fe3ec69cb798be33c7fb64dfb716244
“WilmerMN” <wilmermn369@gmail.com>
2023-03-01 14:22:51 -0500

    test one something

--- JavaScript/Variable.js
+++ JavaScript/Variable.js
@@ -1,29 +1,95 @@
+
+
 var vName = "Wilmer";
 console.log(vName);
 console.log(typeof(vName));
+
 
 
 var vPi = 3.141592;
 console.log(vPi);
 console.log(typeof(vPi));
 
+
+
 var vNumberIntiger = 29;
 console.log(vNumberIntiger);
 console.log(typeof(vNumberIntiger));
 
+
+
 var vTrue = true
 var vFalse = false
+
 console.log(vTrue, vFalse);
 console.log(typeof(vTrue, vFalse));
+
+var vN1 = 1;
+var vN2 = 2;
+/*var vRespuestaT;
+var vRespuestaF;*/
+
+/*if (RespuestaT)
+    {vN2 < vN1}
+else (vRespuestaF) 
+    {vN1 < vN2}
+
+console.log(vRespuestaF);*/
+
+
+
+
 
 var vNull = null
 console.log(vNull);
 console.log(typeof(vNull));
 
 /*var vOldYear = 21;*/
-var vPerson = { vName , vOldYear: 21 };
+var vPerson = { vName2: "Jose", vOldYear: 21 };
 console.log(vPerson);
-console.log(typeof(vPerson))
+console.log(typeof(vPerson));
+
+var vArrays = [1, 2, 3, 4, 5];
+console.log(vArrays);
+console.log(typeof(vArrays));
+
+/*-----------PRUEBA DE CODIGO------------*/
+//Prueba de lineas
+
+const vYearOld = 25;
+/*vYearOld = "Prueba"*/;
+console.log(vYearOld);
+console.log(typeof(vYearOld));
+
+let vUndefined;
+console.log(vUndefined);
+console.log(typeof(vUndefined));
+
+var vSum
+var vN01, vN02;
+    vN01 = 3
+    vN02 = 2
+    vSum = vN01 + vN02
+    console.log(vSum);
+
+    let vObject = {vName01: "Wilmer M.", vDNI: 101015101015, vAcces: true};
+    console.log(vObject["vDNI"]);
+    console.log(vObject.vName01);
+    
+    let vArrays01 = [1, "Hi", {vObject: "Wilmer M"}]
 
 
+var vText = "2"
+    let vDobleSuma = vN01 !== vN02;
+    console.log(vDobleSuma);
 
+    let vDobleSumaa = vN02 !== vText;
+    console.log(vDobleSumaa);
+
+    let vVariable = "";
+    vVariable = prompt("ingrese su nombre");
+
+    var vNumeroPorTeclado = parseInt (prompt("Ingrese un numero"));
+    console.log(vNumeroPorTeclado)
+
+    /*isNaN - es verdadero o falso*/
