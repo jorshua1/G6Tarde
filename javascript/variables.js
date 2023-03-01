480a9cd274ff19d7810c491c47a67a5225c8445d
Tatiana Paredes <tatianaparedescalderon@gmail.com>
2023-03-01 15:01:48 -0500

    se añadio ejercicio en clase de variables

---
+++ javascript/variables.js
@@ -0,0 +1,22 @@
+let nombre ="Tatiana";
+console.log(nombre)
+console.log (typeof(nombre));
+let pi=3.141592;
+console.log(pi);
+console.log (typeof(pi));
+let verdadero=!!(1);
+console.log(verdadero);
+console.log(typeof(verdadero))
+let indefinido;
+console.log(indefinido);
+console.log(typeof(indefinido));
+const numero=15;
+// comentario de una linea
+/*
+comentario
+multilinea
+*/
+let objeto={nombre:"Miguel",
+            cedula:1098777777,
+            acceso:true}
+console.log(objeto.acceso)
