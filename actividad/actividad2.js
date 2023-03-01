5a0bb060e944b66ece0393bed59367157778f38c
Alejandro <castillocazorla05@gmail.com>
2023-03-01 17:38:38 -0500

    taller uno

---
+++ actividad/actividad2.js
@@ -0,0 +1,7 @@
+let capital=parseInt(prompt("ingrese su capital"))
+
+ganancia= capital*0.012
+
+alert("su pago sera de"+ganancia)
+
+
---
+++ actividad/actividad5.js
@@ -0,0 +1,14 @@
+let notaA= parseInt(prompt("ingrese la nota del estudiante"))
+let notaB= parseInt(prompt("ingrese la nota 2 del estudiante"))
+let notaC= parseInt(prompt("ingrese la nota 3 del estudiante"))
+let notaD= parseInt(prompt("ingrese la nota 4 del estudiante"))
+
+sumaNotas=(notaA+notaB)/2
+sumaNotas2=(notaC+notaD)/2
+
+result= sumaNotas*0.4
+result2=sumaNotas2*0.6
+
+resultado=result+result2
+alert("su promedi es de" + resultado)
+
