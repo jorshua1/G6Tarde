e708954bb84449d00f7e076bf7cb1e02a5ac8afb
sergioArdila <sergiochecho15@hotmail.com>
2023-03-01 15:02:01 -0500

    clase de operadores y variables

---
+++ javascript/variables.js
@@ -0,0 +1,62 @@
+var nombre = "sergio";
+
+
+
+console.log(nombre);
+console.log(typeof(nombre));
+
+
+var pi = 3.141592
+console.log(pi);
+console.log(typeof(pi));
+
+
+//console.log(edad);
+//console.log(indefinido);
+
+edad="hola";
+
+
+
+//tipos de variables
+
+let cadena = "esto es un strig";
+let entero = 155;
+let decimal = 123.123;
+let verdad = true;
+let falso = false;
+let nulo = null;
+let indefinido;
+let objeto = { nombre : "sergio", cedula:1234338289, acceso:true}
+
+
+// esto es un comentario unilinea
+
+/* 
+esto es un comentario multi linea
+*/
+
+// operadores de asignacion 
+const variablealfa =5;
+let variablebeta = "hola";
+variablebeta = 10;
+
+//operadores de incremento o decremento.
+
+//incremento
+let contador =0;
+contador++;
+//console-log(contador);
+//decremento
+let contador1 = 5;
+contador1--;
+console.log(contador1);
+
+x=10;
+x = x / 5;
+
+console.log(x)
+
+
+var num1= prompt("ingrese un numero");
+
