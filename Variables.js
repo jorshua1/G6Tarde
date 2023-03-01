f3758ecc28bee7516f645409e1878f9dc3743e9c
Zarela <ninozarela@gmail.com>
2023-03-01 15:10:58 -0500

    Ejercicio 1

---
+++ Variables.js
@@ -0,0 +1,29 @@
+/*var nombre = "Zarela";
+console.log(nombre);
+console.log(typeof(nombre));
+var pi = 3.141592;
+console.log(pi);
+console.log(typeof(pi));
+
+const numero = 17;
+// Las constantes no se pueden cambiar
+/* Comentario
+en 
+varias
+lineas */
+//numero = "Hola"
+//let indefinido;//
+//console.log(indefinido);
+////////////////////////////*/
+var numa = 12;
+var numb = 10;
+numa ||= numb;
+console.log(numa);
+
+
+
+let nulo = null;
+console.log(nulo)
+let objeto = {nombre : "Zarela",
+              cedula : 1005602008,
+              acceso : true}
