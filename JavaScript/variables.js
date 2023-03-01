e99609a6529df6d679da9000fbd1eff9b47f97a3
michaelAfanador <maicolcapacho12@gmail.com>
2023-03-01 15:04:14 -0500

    se agrego el archivo de variables.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,69 @@
+var nombre = "michael";
+console.log(nombre);
+console.log(typeof(nombre));
+var pi = "3.141592";
+console.log(pi);
+console.log(typeof(pi));
+
+//esto es un comentario
+/*esto 
+es 
+comentario
+de 
+multi-linea
+ */
+
+
+//const edad = 22
+edad = "hola"
+
+//let indefinido 
+//console-log(indefinido);
+
+let cadena = "esto es un string";
+let entero = 155;
+let decimal = 152.54;
+console.log(typeof decimal);
+let verdad =  true; //true/false 
+let nulo = null;
+let indefinido;
+let objeto = {nombre:"michael",
+                cedula: 196062942,
+                acceso : true}
+console.log(objeto["nombre"])
+
+let arreglo = [1,"hola",{clave: objeto.nombre},false,null]
+arreglo[1] = "ud me cae mal"
+console.log(arreglo[1])
+
+var num1 = 3;
+var num2 = 2;
+
+var resta = num1 - num2;
+
+console.log(resta);
+
+var num3 = 14
+var num4 = 7
+
+var division = num3 / num4
+console.log(division)
+let var1 = 50
+let var2 = 60
+let var3 = var1>=var2
+console.log(var3)
+
+var a = 34
+var b = 14
+
+var c = a<=b
+console.log(c)
+
+console.log(a==b)
+
+console.log (a<b)
+
+console.log (a>b)
+
+var NumeroPorTeclado = prompt ("ingrese un nombre")
+
