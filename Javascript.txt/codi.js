31eb43cc12c4888f6aa5d464995b9cb394868624
Alejandro <castillocazorla05@gmail.com>
2023-03-01 17:49:02 -0500

    ejemplos

---
+++ Javascript.txt/codi.js
@@ -0,0 +1,12 @@
+
+
+//ejercicio dos
+
+let distance= parseInt(prompt["ingrese la distancia en kilometros"])
+let velocidad1= parseInt(prompt["ingrese la velocidad1"])
+let velocidad2= parseInt(prompt["ingrese la velocidad2"])
+
+tiempo= distance/(velocidad1*velocidad2)
+tiempo=tiempo*60
+
+console.log(tiempo)
