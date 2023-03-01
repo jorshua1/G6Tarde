e7041b781fa6ee69da42ae2e86828ba7184a5550
Jefphunter <jefphunter@gmail.com>
2023-03-01 17:59:56 -0500

    Miercoles 01 Marzo 2023 18:00

---
+++ tallerUno/ejercicio-1.js
@@ -0,0 +1,21 @@
+//*Evaluar las siguientes expresiones A=2, B=5, C=1://
+
+//I. 3*A-4*B/A**2//
+
+let A = 2;
+let B = 5;
+let C = 1;
+
+ecuacion1 = ((3 * A) - (4 * B)) / A ** 2;
+ecuacion2 = 3 * A - 4 * B / A ** 2;
+
+console.log(ecuacion1);
+console.log(ecuacion2);
+
+//II. (((B+C)/2*A+10)*3*B)-6//
+
+ecuacion3 = (((B+C)/2 * A + 10) * 3 * B) - 6;
+ecuacion4 = B+C/2 * A + 10 * 3 * B - 6;
+
+console.log(ecuacion3);
+console.log(ecuacion4);
---
+++ tallerUno/ejercicio-2.js
@@ -0,0 +1,9 @@
+//un individuo desea invertir su capital en un banco y desea saber cuanto dinero ganará//
+//después de un mes si el banco paga razón de  1.2% mensual//
+
+let capitalInvertido;
+let porcentajeBanco = 1.2;
+
+ganancia = capitalInvertido*(porcentajeBanco/100);
+
+console.log(ganancia);
