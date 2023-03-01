d0c6284d3f16a71f8fb6f30b9a4238e45e930988
Jay <danielpinzon20jay@gmail.com>
2023-03-01 17:24:40 -0500

    taller 1 01-03-2023

--- JavaScript/Taller1/punto7.js
+++ JavaScript/Taller1/punto7.js
@@ -3,6 +3,6 @@ var venta1= parseInt(prompt("Ingrese valor venta 1"));
 var venta2= parseInt(prompt("Ingrese valor venta 2"));
 var venta3= parseInt(prompt("Ingrese valor venta 3"));
 var comision=((venta1+venta2+venta3)*10)/100;
-var salarioTotal=sueldoBase+comision
+var salarioTotal=sueldoBase+comision;
 alert("usted tiene una comision total de:"+comision);
 alert("Su salario total del mes e de:"+salarioTotal);
