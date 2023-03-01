cc522ff5c007a09bf677b4ad4c0377012c3e275b
Nicolasriaño <mnegocios339@gmail.com>
2023-03-01 15:07:29 -0500

    lo que hice ayer

---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+var nombre = "Nicolas"
+console.log(nombre) 
+/* esto es un comentario de linea*/
+const edad = 16
+let indefinido;
+console.log(indefinido);
+var num1 = 2
+var num2 = 56
+var resultado = num1*num2
+console.log(resultado) 
+let cadena = "esto es un simulacro";
+let entero = 777;
+let decimal = 777.54;
+console.log(decimal)
+let verdad =true;
+let nulo = null;
+let objeto = {nombre:"Nicolas",
+ clave: "1097493073",
+ acceso: true} 
+ console.log
+ 
