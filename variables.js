062ceba1fff585e1bdb6d63c484a57dfd79136ae
santiago <santa051011@gmail.com>
2023-03-01 15:09:05 -0500

    se agregaron los archivos

---
+++ variables.js
@@ -0,0 +1,32 @@
+var nombre = "santiago";
+console.log(nombre);
+console.log(typeof(nombre));
+var pi = 3.141592;
+console.log(pi);
+console.log(typeof(pi))
+const edad = 17
+
+
+
+let cadena = "esto es un string";
+let entero = 155;
+let decimal =152.54;
+console.log(typeof decimal);
+let verdad = true; // true/false
+let nulo = null;
+let indefinido;
+console.log(indefinido)
+let objeto = {nombre: "saantiago",
+            cedula: 1097492090,
+            acceso: true}
+console.log(objeto.nombre)
+let arreglo = [1, "hola", {clave: objeto.nombre}, false, null]
+
+console.log(arreglo[1])
+
+let num1 = 18
+let num2 = 4
+
+let division = num1 / num2;
+
+console.log(division);
