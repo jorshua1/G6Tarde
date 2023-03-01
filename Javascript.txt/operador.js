fdb8ad9c8b301d7ad8e0de91e5efcd6e1da796d3
Alejandro <castillocazorla05@gmail.com>
2023-03-01 15:02:32 -0500

    operadores

---
+++ Javascript.txt/operador.js
@@ -0,0 +1,25 @@
+let precio = 5
+let cantidad = 6
+let muliplicacion = preciocantidad
+console.log(muliplicacion)
+
+let number3 = 3
+let number2 = 5
+
+
+let cadena = "hola"
+let entero = 122
+let decimal = 3.4
+console.log (typeof decimal) ; entero
+let verdadero = false
+let nulo = null
+let indefinido = undefined;
+console.log (indefinido) ;
+
+// mientras mas clves mas comas
+let objeto = {nombre : "luis", cedula : 31252120, acceso :true}
+
+console.log (objeto.nombre) 
+
+let arreglo = [1, "hols", {clave: objeto.nombre},false, true, null];
+console.log (arreglo[1])
---
+++ Javascript.txt/variable.js
@@ -0,0 +1,9 @@
+
+let odjeto = {nombre3:"juan", numero:300, pais: "francia"}
+console.log(odjeto.nombre3)
+let precio= 5600
+let cantidad=20
+valor= cantidad*precio
+console.log(valor)
+precio= valor/cantidad
+console.log()
