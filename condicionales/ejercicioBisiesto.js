8b58df798c2c2084cb9b4d0f9b2ca223ea9c42bf
Gore333 <diegoespinel94@gmail.com>
2023-03-04 12:18:37 -0500

    tema- condicionales

---
+++ condicionales/ejercicioBisiesto.js
@@ -0,0 +1,9 @@
+let año = Number(prompt("Ingrese un año"));
+
+if (isNaN(año)|| año <=0){
+    console.log("Ingrese un dato valido");
+}else if (año %100 ===0 && año %400===0 && año%4===0){
+    console.log("El año es bisiesto")
+}else{
+    console.log("El año no es bisiesto");
+}
