2fc609af0153743e534428b765600183a4dd710b
Nicolas Ruiz <nicosandr22@gmail.com>
2023-03-03 22:00:16 -0500

    commit ejercicio condicional de descuento

--- javaScript/Condicionales/CondicionalDescuento.js
+++ javaScript/Condicionales/CondicionalDescuento.js
@@ -1,1 +1,14 @@
-let valor = parseFloat(prompt("Ingrese el valor de la compra"))
+let valor = parseFloat(prompt("Ingrese el valor de la compra"));
+let descuento = valor * 0.3;
+if (isNaN(valor)){
+    alert("Ingrese un valor valido")
+}else if(valor >= 500000){
+    valor = valor - descuento;
+    alert("El valor del descuento es de: " + descuento)
+    alert("El valor con el descuento aplicado es de: " + valor)
+}else {
+    descuento = valor * 0.15;
+    valor = valor - descuento;
+    alert("El valor del descuento es de: " + descuento)
+    alert("El valor con el descuento aplicado es de: " + valor)
+}
