2467a9d14b7142e36ae7996bc36c54f422bc9575
Miguelit09 <miguel.p0908@gmail.com>
2023-03-05 16:01:46 -0500

    se hicieron cambios en condicionales

--- javascript/condicionales/compras.js
+++ javascript/condicionales/compras.js
@@ -2,14 +2,14 @@ let valorCompra = Number(prompt("¿Cuál fue el valor de la compra?"));
 let descuento = 0;
 let valorTotal = 0;
 
-if(isNaN(valorCompra)) || valorCompra<=0{
+if(isNaN(valorCompra) || valorCompra<=0){
     alert("Ingrese un valor de compra válido");
-}if(valorCompra>=500000){
+}else if(valorCompra>=500000){
     descuento = valorCompra*0.3;
     valorTotal = valorCompra-descuento;
-    alert("El valor final de la compra fue: " + valorTotal + " - tuvo un descuento del 30%");
+    alert("El valor final de la compra fue: " + valorTotal + "$ - tuvo un descuento de: " + descuento + "$");
 }else{
     descuento = valorCompra*0.15;
     valorTotal = valorCompra-descuento;
-    alert("El valor final de la compra fue: " + valorTotal + " - tuvo un descuento del 15%");
+    alert("El valor final de la compra fue: " + valorTotal + "$ - tuvo un descuento de: " + descuento + "$");
 }
---
+++ javascript/condicionales/transportePaquetes.js
@@ -0,0 +1,31 @@
+//Algoritmo para determinar el costo del transporte de un paquete dependiendo de su lugar de destino y su peso
+
+let pesoPaquete = Number(prompt("¿Cuánto pesa en kg el paquete a transportar?"));
+let zonaDestino = 0;
+let cobroEntrega = 0;
+
+if(isNaN(pesoPaquete)|| pesoPaquete<=0){
+    alert("Por favor, ingrese un valor de peso válido");
+}else if(pesoPaquete>5){
+    alert("Lo sentimos, por medidas de seguridad, no podemos transportar paquetes de más de 5kg");
+}else{
+    zonaDestino = Number(prompt("¿A qué zona es el envío? Seleccione el número de la opción: 1. América del Norte -- 2. América Central -- 3. America del Sur -- 4. Europa -- 5. Asia"))
+    if(isNaN(zonaDestino) ||zonaDestino<=0 || zonaDestino>=6){
+        alert("Por favor, ingrese una zona de destino válida (Un número del 1 al 5)");
+    }else if(zonaDestino === 1){
+        cobroEntrega = pesoPaquete*24
+        alert("El costo del transporte del paquete a América del Norte es de " + cobroEntrega + "$");
+    }else if(zonaDestino === 2){
+        cobroEntrega = pesoPaquete*20
+        alert("El costo del transporte del paquete a América Central es de " + cobroEntrega + "$");
+    }else if(zonaDestino === 3){
+        cobroEntrega = pesoPaquete*21
+        alert("El costo del transporte del paquete a América del Sur es de " + cobroEntrega + "$");
+    }else if(zonaDestino === 4){
+        cobroEntrega = pesoPaquete*10
+        alert("El costo del transporte del paquete a Europa es de " + cobroEntrega + "$");
+    }else if(zonaDestino === 5){
+        cobroEntrega = pesoPaquete*18
+        alert("El costo del transporte del paquete a Asia es de " + cobroEntrega + "$");
+    }
+}
