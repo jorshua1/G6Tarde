7222509bad536d8fa193afe846275d2d029d5e95
Diego <dtp3124494488t@gmail.com>
2023-03-06 17:45:03 -0500

    mas ejercicios

--- javascript/condicionales.js
+++ javascript/condicionales.js
@@ -92,10 +92,166 @@ if (isNaN(compra) || compra < 0) {
   if (compra >= 500000) {
     let desc = compra * 0.3;
     console.log("el descuento es: " + desc);
+    let compraT = compra - desc;
+    console.log("La compra con el descuento es: " + compraT);
   } else {
     if (compra < 500000) {
       let desc = compra * 0.15;
       console.log("el descuento es: " + desc);
+      let compraT = compra - desc;
+      console.log("La compra con el descuento es: " + compraT);
     }
   }
 }
+
+//ejercicio Daniel Jay 
+let compras = parseInt(prompt("Ingrese el valor de su compra"))
+
+if (isNaN(compras) || compras < 0) {
+  console.log("Digite un valor de compra valido");
+} else {
+  if (compras >= 250000) {
+    let descuento = compras * 0.3;
+    let total = compras - descuento;
+    console.log("El valor a pagar es: " + total);
+  } else {
+    if (compras >= 200000) {
+      let descuento = compras * 0.2;
+      let total = compras - descuento;
+      console.log("El valor a pagar es: " + total);
+    } else {
+      if (compras < 200000 && compras >= 50000) {
+        console.log("Su compra no aplica para descuento bruto")
+      } else {
+        let total = compras + 2000;
+        console.log("El valor a pagar es: " +total);
+      }
+    }
+  }
+}
+
+//ejercicio de Nicolas Ruiz
+
+let lado1 = parseInt(prompt("Ingrese el primer lado"))
+let lado2 = parseInt(prompt("Ingrese el segundo lado"))
+let lado3 = parseInt(prompt("Ingrese el tercer lado"))
+if (isNaN(lado1) || lado1 < 0 || isNaN(lado2) || lado2 < 0 || isNaN(lado3) || lado3 < 0) {
+  console.log("Ingrese un valor de lados validos bizco");
+} else {
+  if (lado1 == lado2 && lado1 == lado3) {
+    console.log("Es un triangulo Equilatero");
+  } else {
+    if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+      console.log("El triangulo es Isosceles");
+    } else {
+      console.log("El triangulo es Escaleno");
+    }
+  }
+}
+
+//ejercicio estacion de año
+
+let mes = parseInt(prompt("Ingrese el numero del mes en el que se encuentra"))
+if (isNaN(mes) || mes < 0 || mes >= 13) {
+  console.log("Escriba un numero de mes valido");
+} else {
+  if (mes == 12 || mes == 1 || mes == 2) {
+    console.log("La estacion del mes que digito es Invierno");
+  } else {
+    if (mes == 3 || mes == 4 || mes == 5) {
+      console.log("La estacion del mes que digito es Primavera");
+    } else {
+      if (mes == 6 || mes == 7 || mes == 8) {
+        console.log("La estacion del mes que digito es Verano");
+      } else {
+        console.log("La estacion del mes que esta es Otoño");
+      }
+    }
+  }
+}
+
+//EJERCICIO DE TRIBUTAR
+
+let años = parseInt(prompt("Digite su edad"));
+let sueldo = parseInt(prompt("Digite su ingreso mensual en euros"));
+
+if (isNaN(años) || años < 0) {
+  console.log("Ingrese una edad valida");
+} else {
+  if (años > 16 && sueldo >= 1000) {
+    console.log("Debe tributar por feo");
+  } else {
+    console.log("No debe tributar");
+  }
+}
+
+//ejercicio sin sentido de genero
+
+let genero = prompt("Ingrese su genero");
+if (genero === "Hombre" || genero === "HOMBRE" || genero === "hombre") {
+  console.log("Su genero es: " + genero);
+} else {
+  if (genero === "Mujer" || genero === "MUJER" || genero === "mujer") {
+    console.log("Su genero es: " + genero);
+  } else {
+    if (genero === "Indefinido" || genero === "INDEFINIDO" || genero === "indefinido") {
+      console.log("Su genero es: " + genero);
+    } else {
+      console.log("Digite un valor de genero valido");
+    }
+  }
+}
+
+//ejercicio de andres rueda
+let sexo = prompt("Digite su genero, Mujer o Hombre");
+let name = prompt("Digite su nombre");
+
+if (sexo === "Mujer") {
+  if (name[0] === "A" || name[0] === "B" || name[0] === "C" || name[0] === "D" || name[0] === "E" || name[0] === "F" || name[0] === "G" || name[0] === "H" || name[0] === "I" || name[0] === "J" || name[0] === "K" || name[0] === "L") {
+    console.log("Ud pertenece al grupo A");
+  } else {
+    if (name[0] === "M" || name[0] === "N" || name[0] === "Ñ" || name[0] === "O" || name[0] === "P" || name[0] === "Q" || name[0] === "R" || name[0] === "S" || name[0] === "T" || name[0] === "U" || name[0] === "V" || name[0] === "W" || name[0] === "X" || name[0] === "Y" || name[0] === "Z") {
+      console.log("Ud pertenece al grupo B");
+    } else {
+      console.log("Escriba bien sirilo");
+    }
+  }
+} else {
+  console.log("Digite un genero valido");
+}
+
+if (sexo === "Hombre") {
+  if (name[0] === "A" || name[0] === "B" || name[0] === "C" || name[0] === "D" || name[0] === "E" || name[0] === "F" || name[0] === "G" || name[0] === "H" || name[0] === "I" || name[0] === "J" || name[0] === "K" || name[0] === "L") {
+    console.log("Ud pertenece al grupo B");
+  } else {
+    if (name[0] === "M" || name[0] === "N" || name[0] === "Ñ" || name[0] === "O" || name[0] === "P" || name[0] === "Q" || name[0] === "R" || name[0] === "S" || name[0] === "T" || name[0] === "U" || name[0] === "V" || name[0] === "W" || name[0] === "X" || name[0] === "Y" || name[0] === "Z") {
+      console.log("Ud pertenece al grupo A");
+    } else {
+      console.log("Escriba bien sirilo");
+    }
+  }
+} else {
+  console.log("Digite un genero valido");
+}
+
+//ejercicio finanzas
+
+let monto = parseInt(prompt("Ingrese el valor del monto de la finanza"))
+
+if (isNaN(monto) || monto < 0) {
+  console.log("Digite un valor de monto valido");
+} else {
+  if (monto < 50000) {
+    let montoT = monto * 0.3;
+    console.log("La cuota a pagar es de: " +montoT);
+  } else {
+    if (monto > 50000) {
+      let montoT = monto * 0.2;
+      console.log("La cuota a pagar es de: " +montoT);
+    } else {
+      console.log("No debe pagar cuota");
+    }
+  }
+}
+
+//
