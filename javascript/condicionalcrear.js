4b5dc2152f48122b49bf7de4194ac3772f0e835f
Nicolasriaño <mnegocios339@gmail.com>
2023-03-06 15:16:44 -0500

    .

---
+++ javascript/condicionalcrear.js
@@ -0,0 +1,8 @@
+var numero = prompt("Ingrese un número:");
+if (numero > 0) {
+  console.log(numero + " es un número positivo");
+} else if (numero < 0) {
+  console.log(numero + " es un número negativo");
+} else {
+  console.log("El número ingresado es cero");
+}
---
+++ javascript/condicionales2.js
@@ -0,0 +1,16 @@
+let edad = Number(prompt("digite su edad"))
+
+if(isNaN(edad)){
+    console.log("error")  
+   }else if(edad<=0){
+        console.log("ERROR")
+    }    
+    else if(edad<=0){
+        console.log("ERROR")
+        
+    
+    }else if(edad >=1 && edad<=17){
+    console.log("usted es menor de edad")
+    }else if(edad>=18){
+    console.log("usted es mayor de edad")
+}
