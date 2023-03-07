b7baca12bd1202239af32ec09394f035e95d8b95
Jonathan <jcarrillo366@misena.edu.co>
2023-03-07 17:35:39 -0500

    condicionales ejercicios

--- condicionales/condicionalestaller.js
+++ condicionales/condicionalestaller.js
@@ -188,5 +188,121 @@ if(isNaN(mes)|| mes<0 || mes>12){
 /*determinar el tipo de triangulo segun la medida de sus lados: escaleno, iscoceles o equilatero*/
 var base = parseInt(prompt("ingrese la base del triaungulo en cm"));
 var a = parseInt(prompt("ingrese el primer lado")); 
-var c = parseInt
+var c = parseInt(prompt("ingrese el segundo lado"));
+if(base==a && a==c && c==base){
+    console.log("es un triangulo equilatero")
+}else if(a==c && base!==c){
+    console.log("es un triangulo isosceles")
+}else{
+    console.log("es un triangulo escaleno")
+}
+/*Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o
+ superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales 
+ y muestre por pantalla si el usuario tiene que tributar o no.*/
+ var edad = parseInt(prompt("ingrese su edad"));
+ var ingresos = parseInt(prompt("ingrese sus ingresos mensuales en euros"));
+ if(edad>=16 && ingresos>=1000){
+    console.log("debe tributar")
+ }else{
+    console.log("no debe tributar")
+ }
+/*Se necesita registrar el género de la persona que ingresé; el cuál solo acepte, hombre, mujer e indefinido 
+además debe leerlos tanto la palabra en mayúscula y en minúscula, además solo deberá leer esos tres géneros 
+en caso de agregar algo que no sea eso deberá aparecer un mensaje que diga "dijite un valor válido", y si ha 
+ingresado el género dar un mensaje con el género que ingreso*/
+var genero = prompt("teclee un numero segun su genero 1 = hombre 2 = mujer 3 =indefinido");
+if(isNaN(genero) || genero<0 || genero>=4){
+    console.log("dijite un valor valido entre 1-3")
+}else if(genero==1){
+    console.log("Hombre")
+}else if(genero==2){
+    console.log("Mujer")
+}else{
+    console.log("indefinido")
+}
+/*Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo y el nombre. 
+El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un 
+nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte al usuario 
+su nombre y sexo, y muestre por pantalla el grupo que le corresponde.*/
+
+var nombre = prompt("escriba su nombre");
+var sexo = prompt("escriba su sexo masculino(M) o femenino(F)");
+if(nombre<=0 || nombre>=0 || sexo<=0 || sexo>=0){
+    console.log("error datos invalidos")
+}else if(((nombre[0]<"M" || nombre[0]<"m") && (sexo=="F" || sexo=="f")) || ((nombre[0]>"N" || nombre[0]>"n") && (sexo=="M" || sexo=="m"))){
+    alert("pertene al grupo A")
+}else if((((nombre[0]>"M" && nombre[0]<"Z") || (nombre[0]>"m" && nombre[0]<"z")) && (sexo=="F" || sexo=="f")) || (((nombre[0]<"N" && nombre[0]>A) || (nombre[0]<"n" && nombre[0]>a)) && (sexo=="M" || sexo=="m"))){
+    alert("pertenece al grupo B")
+}else{
+    alert("error datos no validos")
+}
+/*Una compañía de seguros está abriendo un dpto de finanzas y estableció un programa para captar clientes,
+ que consiste en lo siguiente: Si el monto por el que se efectúa la fianza es menor que $50 000 la cuota a 
+ pagar será por el 3% del monto, y si el monto es mayor que $50 000 la cuota a pagar será el 2% del monto, 
+ si el monto es igual a $50.000 no se debe pagar cuota. La afianzadora desea determinar si se debe pagar cuota 
+ y cuál será la cuota que debe pagar un cliente.*/
+ var monto = parseInt(prompt("ingrese el monto"));
+ if(isNaN(monto)){
+    console.log("error")
+ }else if(monto<50000 && monto>0){
+ var cuota1 = monto*(3/100)
+ 
+    console.log("la cuota que debe pagar sera del 3% segun su monto el pago es de "+ cuota1)
+ }else if(monto>50000){
+ var cuota2 = monto*(2/100)
+ 
+    console.log("la cuota que debe pagar sera del 2% segun su monto el pago es de "+ cuota2)
+ }else{
+    console.log("no paga cuota")
+ }
+ /*Una tienda aplica un descuento del 15% para las compras que incluyan más de 10 artículos iguales, 
+donde el precio supere los 40 euros. Tu programa debe comprobar y calcular la cantidad a pagar
+sabiendo el número de artículos y el precio de un articulo.*/
+var articulos = parseInt(prompt("ingrese la cantidad de articulos iguales que compro"));
+var precio = parseInt(prompt("ingrese el precio del articulo"));
+if(isNaN(articulos) || isNaN(precio)){
+    console.log("error valores invalidos")
+}else if(articulos>10 && precio>40){
+    var total1 = (articulos*precio)-(articulos*precio)*(15/100)
+    console.log("el valor a pagar con el descuento aplicado del 15% es de "+ total1)
+}else{
+    var total2 = articulos*precio
+    console.log("el valor a pagar es de "+ total2)
+}
+/*Haga un ejercicio con if (y) B)  (Cree un algoritmo, el cual le pida por teclado la edad a un usuario, 
+    seguiido debera asignarle los siguientes rangos, si el usuario tiene entre 0 y 5 años, debera asignarle 
+    la categoria bb, si tiene entre 6 y 10 años, tiene la categoria adolex ente, si tiene entre 11 y 18 años, 
+    debera asignarle la categoria casi maduro, si tiene de 19 a 25 anos, debera asignarle maduro, si tiene de 26 a 50 
+    pertenecera a casi pensionado, si tiene entre 51 y 70, tiene por categoria penxionadox, y si tiene de 71 en adelante 
+    pertenecera al grupo amigo de matusalen, debera mostrarle al usuario en que categoria se encuentra.  )*/
+    var edad = parseInt(prompt("ingrese su edad"));
+    if(isNaN(edad) || edad<0){
+        console.log("error datos invalidos")
+    }else if(edad>0 && edad<=5){
+        console.log("pertenece a la categoria bebé")
+    }else if(edad>=6 && edad<=10){
+        console.log("pertenece a la categoria adolescente")
+    }else if(edad>=11 && edad<=18){
+        console.log("pertenece a la categoria casi maduro")
+    }else if(edad>=19 && edad<=25){
+        console.log("pertenece a la categoria maduro")
+    }else if(edad>=26 && edad<=50){
+        console.log("pertenece a la categoria pensionado")
+    }else if(edad>=51 && edad<=71){
+        console.log("pertenece a la categoria penxionadox")
+    }else{
+        console.log("pertenece a la categoria matusalen")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
