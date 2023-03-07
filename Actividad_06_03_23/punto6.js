9467c4032e9a29838bab59aa8062ed978c61d85b
AndresRueda <andresruedap22@gmail.com>
2023-03-07 17:33:44 -0500

    adelanto

--- Actividad_06_03_23/punto6.js
+++ Actividad_06_03_23/punto6.js
@@ -2,21 +2,14 @@
 El grupo A esta formado por las mujeres con un nombre anterior a la M y los hombres con un nombre posterior a la N 
 y el grupo B por el resto. Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde. "*/
 
-let nombre = prompt("¿Cual es su nombre?(ponerlo en minuscula) ");
 let genero = prompt("¿Cuál es tu sexo (M o H)? ");
-let grupo = "";
+let nombre = prompt("¿Cual es su nombre?(ponerlo en minuscula) ");
 
-if (genero = "M"){
-    if (nombre[0] <= "m"){
-        grupo = "A";
-    }else{
-        grupo = "B";
-}
+if(genero == "M" && nombre[0] < "m" || genero == "H" && nombre[0] > "n"){
+    grupo = "A";
 }else{
-    if (nombre[0] <= "n"){
-        grupo = "A";
-    }else{
-        grupo = "B";
-    }
+    grupo = "B";
 }
+
 alert("Tu grupo es " + grupo);
+
