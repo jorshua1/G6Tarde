b6ee7fefbbd1af4cf79bc5d899bd474906e8a671
Zarela <ninozarela@gmail.com>
2023-03-07 17:34:58 -0500

    Ejercicios clase del martes

--- EjerciciosCompañeros.js
+++ EjerciciosCompañeros.js
@@ -97,4 +97,65 @@ y los hombres con un nombre posterior a la N y el grupo B por el resto. Escribir
 un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el 
 grupo que le corresponde.*/
 
+let nombre = prompt("Esbriba su nombre en mayusculas");
+let sexo = prompt("Escriba M si el alumno es hombre y F si la alumna es mujer");
+
+if (nombre[0]<"M" && sexo == "F"){
+    console.log(nombre, ", Pertenece al grupo A")
+}
+else if(nombre[0]>="M" && sexo == "M"){
+    console.log(nombre, ", Pertenece al grupo A")
+}
+else{
+    console.log(nombre, ", Pertenece al grupo B")
+}
+
+/*Una compañía de seguros está abriendo un dpto de finanzas y estableció un 
+programa para captar clientes, que consiste en lo siguiente: Si el monto por
+el que se efectúa la fianza es menor que $50 000 la cuota a pagar será por 
+el 3% del monto, y si el monto es mayor que $50 000 la cuota a pagar será 
+el 2% del monto, si el monto es igual a $50.000 no se debe pagar cuota. La 
+afianzadora desea determinar si se debe pagar cuota y cuál será la cuota 
+que debe pagar un cliente.*/
+
+let valor = parseInt(prompt("Ingrese su fianza"));
+let cuota1;
+let cuota2;
+if(isNaN(valor)){
+    console.log("Ingrese un dato valido")
+}
+else if(valor<50000){
+    cuota1 = valor * 0.03;
+    console.log("SI DEBE PAGAR CUOTA, ésta es de ", cuota1)
+}
+else if(valor == 50000){
+    console.log("NO DEBE PAGAR CUOTA", cuota1)
+}
+else{
+    cuota2 = valor * 0.02;
+    console.log("SI DEBE PAGAR CUOTA, ésta es de ", cuota2)
+}
+
+/*"Una tienda aplica un descuento del 15% para las compras que incluyan más de 10 artículos iguales, 
+donde el precio supere los 40 euros. Tu programa debe comprobar y calcular la cantidad a pagar
+sabiendo el número de artículos y el precio de un articulo."*/
+
+let numarticulo = parseInt(prompt("Ingrese la cantidad de articulos iguales"));
+let valorarticulo = parseFloat(prompt("Ingrese el valor unitario del articulo"));
+let descuento;
+let total = (valorarticulo*numarticulo);
+if(isNaN(numarticulo)|| numarticulo<0){
+    console.log("Ingrese un valor valido")
+}
+else if(isNaN(valorarticulo)|| valorarticulo<0){
+    console.log("Ingrese un valor valido")
+}
+else if(numarticulo>10 && total>40){
+    descuento = total * 0.15;
+    console.log("Su descuento es de ", descuento, "debe pagar un total de ", (total-descuento))
+}
+else{
+    console.log("No tiene descuento")
+}
+
 
---
+++ EjerciciosCompañerosParte2.js
@@ -0,0 +1,9 @@
+/*Haga un ejercicio con if (y) B)  (Cree un algoritmo, el cual le pida por teclado la edad a un 
+usuario, seguiido debera asignarle los siguientes rangos, si el usuario tiene entre 0 y 5 años, 
+debera asignarle la categoria bb, si tiene entre 6 y 10 años, tiene la categoria adolex ente, 
+si tiene entre 11 y 18 años, debera asignarle la categoria casi maduro, si tiene de 19 a 25 anos, 
+debera asignarle maduro, si tiene de 26 a 50 pertenecera a casi pensionado, si tiene entre 51 y 70, 
+tiene por categoria penxionadox, y si tiene de 71 en adelante pertenecera al grupo amigo de matusalen, 
+debera mostrarle al usuario en que categoria se encuentra)*/
+
+let edad = parseInt(prompt("Ingrese su edad"));
