9e21cd1bf685452e192393644b7b1cf03470ea7f
Nicolas Ruiz <nicosandr22@gmail.com>
2023-03-08 15:39:50 -0500

    carpeta case

---
+++ javaScript/Condicionales/Switch Case/PrimerCase.js
@@ -0,0 +1,16 @@
+let dato = 1;
+
+switch (dato){
+    case 1: 
+    console.log("Ingreso el numero uno")
+    break;
+
+    case "1":
+    consº.log("Ingreso el numero uno como string");
+    break;
+
+    default: 
+
+    console.log("Ingreso un munero que no se encuentra en los casos")
+    break;
+}
