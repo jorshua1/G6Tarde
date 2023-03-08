deb71192cdcb7573d9dda53edab7630ac3d6773b
CarlosGonzalez <carlosgonzalezleal0@gmail.com>
2023-03-08 17:47:41 -0500

    case 4

---
+++ javascrip/ejercicios propuestos/case4.js
@@ -0,0 +1,8 @@
+let num1=Number(prompt("ingrese el numero 1"))
+let num2=Number(prompt("ingrese el numero 2"))
+
+if (num1>num2){
+    console.log(num2, num1);
+}else if (num2>num1){
+    console.log(num1, num2);
+}
