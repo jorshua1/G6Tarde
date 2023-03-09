57c9c7942d13eec8e0c13723758ae8be95936fbb
brayansantamaria <brayan.santamaria.2002@gmail.com>
2023-03-09 17:08:13 -0500

    taller antes de la mision :)

---
+++ javascript/tallerdeentrega/punto2.js
@@ -0,0 +1,21 @@
+let num=Number(prompt("ingrese un numero del 1 al 10"));
+switch (num){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 9:
+    console.log("su numero es impar");
+    break;
+    case 2:
+    case 4:
+    case 6:
+    case 8:
+    console.log("su numero es par");
+    break;
+    default:
+        console.log("ingrese un valor valido")
+}
+
+
+    
