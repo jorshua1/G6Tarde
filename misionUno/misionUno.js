bcf80abdeb0d1a714ce0bc77a66772efba9cfb85
Jefphunter <jefphunter@gmail.com>
2023-03-10 17:19:05 -0500

    identación

--- misionUno/misionUno.js
+++ misionUno/misionUno.js
@@ -23,58 +23,66 @@ mismo y escriba él código.
 */
 
 let examenUno = parseFloat(prompt("Por favor ingrese nota de examen uno."));
-if (isNaN(examenUno) || examenUno<0 || examenUno>5){
+if (isNaN(examenUno) || examenUno < 0 || examenUno > 5) {
     console.log("Por favor ingrese un valor valido")
-}else{
-let examenDos = parseFloat(prompt("Por favor ingrese nota de examen dos."));
-if (isNaN(examenDos) || examenDos<0 || examenDos>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let examenTres = parseFloat(prompt("Por favor ingrese nota de examen tres."));
-if (isNaN(examenTres) || examenTres<0 || examenTres>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let trabajoUno = parseFloat(prompt("Por favor ingrese nota de trabajo uno."));
-if (isNaN(trabajoUno) || trabajoUno<0 || trabajoUno>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let trabajoDos = parseFloat(prompt("Por favor ingrese nota de trabajo dos."));
-if (isNaN(trabajoDos) || trabajoDos<0 || trabajoDos>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let examenFinal = parseFloat(prompt("Por favor ingrese nota de examen uno."));
-if (isNaN(examenFinal) || examenFinal<0 || examenFinal>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let autoevalEstudiante = parseFloat(prompt("Por favor ingrese nota de autoevaluación estudiante."));
-if (isNaN(autoevalEstudiante) || autoevalEstudiante<0 || autoevalEstudiante>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let autoevalProfesor = parseFloat(prompt("Por favor ingrese nota de eco-evaluación profesor."));
-if (isNaN(autoevalProfesor) || autoevalProfesor<0 || autoevalProfesor>5){
-    console.log("Por favor ingrese un valor valido")
-}else{
-let promExamenes = (((examenUno + examenDos + examenTres)/3)/100)*55;
-let promTrabajos = (((trabajoUno + trabajoDos)/2)/100)*15;
-let promExamenFinal = (examenFinal/100)*20;
-let promAutoEval = (((autoevalEstudiante + autoevalProfesor)/2)/100)*10;
+} else {
+    let examenDos = parseFloat(prompt("Por favor ingrese nota de examen dos."));
+    if (isNaN(examenDos) || examenDos < 0 || examenDos > 5) {
+        console.log("Por favor ingrese un valor valido")
+    } else {
+        let examenTres = parseFloat(prompt("Por favor ingrese nota de examen tres."));
+        if (isNaN(examenTres) || examenTres < 0 || examenTres > 5) {
+            console.log("Por favor ingrese un valor valido")
+        } else {
+            let trabajoUno = parseFloat(prompt("Por favor ingrese nota de trabajo uno."));
+            if (isNaN(trabajoUno) || trabajoUno < 0 || trabajoUno > 5) {
+                console.log("Por favor ingrese un valor valido")
+            } else {
+                let trabajoDos = parseFloat(prompt("Por favor ingrese nota de trabajo dos."));
+                if (isNaN(trabajoDos) || trabajoDos < 0 || trabajoDos > 5) {
+                    console.log("Por favor ingrese un valor valido")
+                } else {
+                    let examenFinal = parseFloat(prompt("Por favor ingrese nota de examen uno."));
+                    if (isNaN(examenFinal) || examenFinal < 0 || examenFinal > 5) {
+                        console.log("Por favor ingrese un valor valido")
+                    } else {
+                        let autoevalEstudiante = parseFloat(prompt("Por favor ingrese nota de autoevaluación estudiante."));
+                        if (isNaN(autoevalEstudiante) || autoevalEstudiante < 0 || autoevalEstudiante > 5) {
+                            console.log("Por favor ingrese un valor valido")
+                        } else {
+                            let autoevalProfesor = parseFloat(prompt("Por favor ingrese nota de eco-evaluación profesor."));
+                            if (isNaN(autoevalProfesor) || autoevalProfesor < 0 || autoevalProfesor > 5) {
+                                console.log("Por favor ingrese un valor valido")
+                            } else {
+                                let promExamenes = (((examenUno + examenDos + examenTres) / 3) / 100) * 55;
+                                let promTrabajos = (((trabajoUno + trabajoDos) / 2) / 100) * 15;
+                                let promExamenFinal = (examenFinal / 100) * 20;
+                                let promAutoEval = (((autoevalEstudiante + autoevalProfesor) / 2) / 100) * 10;
 
-let promedioEstudiante = (promExamenes + promTrabajos + promExamenFinal + promAutoEval);
+                                let promedioEstudiante = (promExamenes + promTrabajos + promExamenFinal + promAutoEval);
 
-console.log(" la nota del estudiante es : " + promedioEstudiante);
+                                console.log(" la nota del estudiante es : " + promedioEstudiante);
 
-if(examenUno <0 || examenDos<0 || examenTres <0 || trabajoUno<0 || trabajoDos<0 || examenFinal<0 || autoevalEstudiante<0 || autoevalProfesor<0 || examenUno >5 || examenDos>5 || examenTres >5 || trabajoUno>5 || trabajoDos>5 || examenFinal>5 || autoevalEstudiante>5 || autoevalProfesor>5){
-    console.log("Por favor ingre un valor valido"); 
-}else if(promedioEstudiante>=0 && promedioEstudiante<=1.9){
-    console.log("Muy bajo");
-}else if(promedioEstudiante>=2 && promedioEstudiante<=2.9){
-    console.log("Bajo");
-}else if (promedioEstudiante>=3 && promedioEstudiante<=3.9){
-    console.log("Básico");
-}else if (promedioEstudiante>=4 && promedioEstudiante<=4.6){
-    console.log("Alto");
-}else if(promedioEstudiante>=4.7 && promedioEstudiante<=5){
-    console.log("Superior");
-}else{
+                                if (examenUno < 0 || examenDos < 0 || examenTres < 0 || trabajoUno < 0 || trabajoDos < 0 || examenFinal < 0 || autoevalEstudiante < 0 || autoevalProfesor < 0 || examenUno > 5 || examenDos > 5 || examenTres > 5 || trabajoUno > 5 || trabajoDos > 5 || examenFinal > 5 || autoevalEstudiante > 5 || autoevalProfesor > 5) {
+                                    console.log("Por favor ingre un valor valido");
+                                } else if (promedioEstudiante >= 0 && promedioEstudiante <= 1.9) {
+                                    console.log("Muy bajo");
+                                } else if (promedioEstudiante >= 2 && promedioEstudiante <= 2.9) {
+                                    console.log("Bajo");
+                                } else if (promedioEstudiante >= 3 && promedioEstudiante <= 3.9) {
+                                    console.log("Básico");
+                                } else if (promedioEstudiante >= 4 && promedioEstudiante <= 4.6) {
+                                    console.log("Alto");
+                                } else if (promedioEstudiante >= 4.7 && promedioEstudiante <= 5) {
+                                    console.log("Superior");
+                                } else {
 
-}}}}}}}}}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
