b7835473742060bc48dcccbd20d30dd57dbf3cf6
Jay <danielpinzon20jay@gmail.com>
2023-03-10 16:21:35 -0500

    Se agrego Mision 2

--- JavaScript/mision2/mision2.js
+++ JavaScript/mision2/mision2.js
@@ -1,39 +1,39 @@
 let eva1=parseFloat(prompt("Ingrese nota evaluacion 1"));
-if (isNaN(eva1) || eva1>5){
+if (isNaN(eva1) || eva1>5 || eva1<0){
     alert("Error, digite la nota en el rango de 1 a 5")
 }else{
     let eva2=parseFloat(prompt("Ingrese nota evaluacion 2"));
-    if (isNaN(eva2) || eva2>5){
+    if (isNaN(eva2) || eva2>5 || eva2<0){
         alert("Error, digite la nota en el rango de 1 a 5")
     }else{
         let eva3=parseFloat(prompt("Ingrese nota evaluacion 3"));
-        if(isNaN(eva3)|| eva3>5){
+        if(isNaN(eva3)|| eva3>5 || eva3<0){
         alert("Error, digite la nota en el rango de 1 a 5")
     }else{
         let promEva=(((eva1+eva2+eva3)/3)*55)/100
         alert("Porcentaje nota de evaluaciones: " + promEva)
         let tra1=parseFloat(prompt("Ingrese nota trabajo1"));
-        if(isNaN(tra1) || tra1>5){
+        if(isNaN(tra1) || tra1>5 || tra1<0){
             alert("Error, digite la nota en el rango de 1 a 5")
         }else{
             let tra2=parseFloat(prompt("Ingrese nota trabajo 2"));
-            if(isNaN(tra2) || tra2>5){
+            if(isNaN(tra2) || tra2>5 || tra2<0){
                 alert("Error, digite la nota en el rango de 1 a 5")
             }else{
                 let promTra=(((tra1+tra2)/2)*15)/100
                 alert("Porcetaje nota trabajos: " + promTra)
                 let examFinal=parseFloat(prompt("Ingrese nota de examen final"));
-                if (isNaN(examFinal) || examFinal>5){
+                if (isNaN(examFinal) || examFinal>5 || examFinal<0){
                     alert("Error, digite la nota en el rango de 1 a 5")
                 }else{
                     let promExam=((examFinal)*20)/100
                     alert("Porcentaje nota examen: " + promExam)
                     let autoEva=parseFloat(prompt("Ingrese nota de auto evaluacion del estudiante"));
-                    if (isNaN(autoEva) || autoEva>5){
+                    if (isNaN(autoEva) || autoEva>5 || autoEva<0){
                         alert("Error, digite la nota en el rango de 1 a 5")
                     }else{
                           let coEva=parseFloat(prompt("Ingrese nota de co evaluacion del profesor"));
-                          if (isNaN(coEva) || coEva>5){
+                          if (isNaN(coEva) || coEva>5 || coEva<0){
                             alert("Error, digite la nota en el rango de 1 a 5")
                           }else{
                             let promAutoEva=((((autoEva+coEva)/2))*10)/100
