c276a960f23bc08e7b7e37f6ca801aec2101c427
Nicolasriaño <mnegocios339@gmail.com>
2023-03-10 15:55:59 -0500

    mision

---
+++ javascript/Taller 1/switch.js
@@ -0,0 +1,27 @@
+let mes =      "febrero";
+switch (mes) {
+    case "diciembre":
+    case "enero":
+    case "febrero":
+         console.log("invierno");
+         break;
+           case "marzo":
+            case "abri":
+            case "mayo":
+            console.log("primavera");
+            break;
+            case "junio":
+            case"julio":
+            case"agosto":
+            console.log ("verano");
+            break;
+            case "septiembre":
+                case "octubre":
+                    case "noviembre":
+                        console.log("otoño");
+                        break;
+                        default:
+                            console.log("no es un mes valido");
+
+
+                        }
