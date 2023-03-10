effd39a5965a4025c36358eba8f0b210444d8de8
Alejandro <castillocazorla05@gmail.com>
2023-03-10 17:20:22 -0500

    primera mision

--- Algoritmos_ejemplos/ejemplo5.js
+++ Algoritmos_ejemplos/ejemplo5.js
@@ -1,10 +1,15 @@
-let genero=(prompt("ingrese el genero"))
+let genero = prompt('ingrese el genero')
 
- if (genero=="Masculino"){
-    alert("Ingresaste Masculino")
-}else if (genero=="Femenino"){
-    alert("ingresaste Femenino")
-}else if (genero=="Indefinido"){
-    alert("ingresaste Indefinido")
-}else if (genero!="Masculino" ,genero!="Femenino", genero!="Indefinido"){
-    alert("ingreso un valor invalido")}
+if (genero == 'Masculino') {
+  alert('Ingresaste Masculino')
+} else if (genero == 'Femenino') {
+  alert('ingresaste Femenino')
+} else if (genero == 'Indefinido') {
+  alert('ingresaste Indefinido')
+} else if (
+  (genero != 'Masculino', genero != 'Femenino', genero != 'Indefinido')
+) {
+  alert('ingreso un valor invalido')
+}
+
+//luis////
