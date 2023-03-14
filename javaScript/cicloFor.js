5cb86c938984dfeec95b9ae2c41f46150dea9de8
Jefphunter <jefphunter@gmail.com>
2023-03-14 17:43:54 -0500

    Ciclos de javaScript

---
+++ javaScript/cicloFor.js
@@ -0,0 +1,16 @@
+/*
+tabla de multiplicar
+*/
+let numeroMultiplicando = 2;/*parseInt(prompt(" Por favor ingrese numero de la tabla que necesita visualizar"));*/
+let numeroMultiplicador = 0;
+
+if (isNaN(numeroMultiplicando)) {
+    while (isNaN(numeroMultiplicando));
+    console.log("Por favor ingrese un valor valido");
+} else {
+    for (i = 0; i <= 10; i++) {
+        numeroMultiplicador++
+        let resultado = numeroMultiplicando * numeroMultiplicador;
+        console.log(numeroMultiplicando + " x " + numeroMultiplicador + " = " + resultado);
+    }
+}
