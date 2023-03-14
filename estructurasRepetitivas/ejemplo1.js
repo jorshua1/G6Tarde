5b79c6d3f1d6f50492a53472ca807d2c0f8b70de
Alejandro <castillocazorla05@gmail.com>
2023-03-14 17:28:11 -0500

    correccion

--- estructurasRepetitivas/ejemplo1.js
+++ estructurasRepetitivas/ejemplo1.js
@@ -1,47 +1,32 @@
-let numero=parseInt(prompt("ingrese  un  numero"))
-let h=1
-if(isNaN(numero)){
-
-    alert("error")
-    }else{
-
-while(numero>0 && h<=10 && h>=0){
-    
-    console.log(numero*h)
+let numero = parseInt(prompt('ingrese  un  numero'))
+let h = 1
+if (isNaN(numero)) {
+  alert('error')
+} else {
+  while (numero > 0 && h <= 10 && h >= 0) {
+    console.log(numero * h)
     h++
-}
-console.log("resultados")
-
-
+  }
+  console.log('resultados')
 }
 
 ////
 
-
-
 ///
-let i=0
+let i = 0
 
-for(i;i<=10;i++){
-    console.log(i)
+for (i; i <= 10; i++) {
+  console.log(i)
 }
 //
 //
 
-let number=parseInt(prompt("ingrese el numero"))
-let m=1
+let number = parseInt(prompt('ingrese el numero'))
+let m = 1
 
-while(isNaN(number)){
-    
-    for(number;number<=10 && number>=0;m++){
-    console.log(number*m)
-
-console.log(resultados)
-
-
+while (isNaN(number)) {
+  number = parseInt(prompt('valor no valido, ingrese un numero'))
 }
-    
-    number=parseInt(prompt("valor no valido, ingrese un numero"))
-
-
- }   
+for (m; m <= 10; m++) {
+  console.log(number * m)
+}
