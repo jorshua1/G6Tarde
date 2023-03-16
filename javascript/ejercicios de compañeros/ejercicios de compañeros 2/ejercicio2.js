a7e857dc1c699b4ed5bdecaf565741790f7d8b1a
brayansantamaria <brayan.santamaria.2002@gmail.com>
2023-03-16 16:52:26 -0500

    ejercicios de compañeros

---
+++ javascript/ejercicios de compañeros/ejercicios de compañeros 2/ejercicio2.js
@@ -0,0 +1,4 @@
+/* mostrar en pantalla los N primero numeros primos.se pide la cantidad de numeros primos
+que se quieren mostrar*/
+
+let cantnumerosprimos=parseInt(prompt("ingrese la cantidad de numeros primos que ver"));
