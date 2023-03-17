355d71a96ea33784a9adca1f6dfbdffc10758261
Gore333 <diegoespinel94@gmail.com>
2023-03-16 20:53:07 -0500

    ejercicio suma de impares

--- Bucles/ejercicios/ejercicio10.js
+++ Bucles/ejercicios/ejercicio10.js
@@ -1,1 +1,14 @@
 //imprima la suma de todos los números impares entre 1 y 100
+
+
+
+var i;
+var suma;
+suma = 0
+
+for (i = 0; i <= 100; i++) {
+    if (i % 2 === 1) {
+        suma = suma + i
+    }
+}
+console.log(suma)
