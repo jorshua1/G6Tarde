23eb1ec7c90005c1e16425df402bfea7621369de
Jefphunter <jefphunter@gmail.com>
2023-03-17 18:05:07 -0500

    Desarrollando Ejercicio tres taller cinco ciclos

--- f-TallerCincoCiclos/ejercicio-3.js
+++ f-TallerCincoCiclos/ejercicio-3.js
@@ -9,29 +9,37 @@ Jóvenes          13 - 29
 Adultos          30 - 59
 Viejos           60 - en adelante
 */
-let numeroPersonas = 1;
+let numeroPersonas = 2;
 personaNiño = 0;
 personaJovenes = 0;
 personaAdultos = 0;
 personaViejos = 0;
 
 for (i = 0; i < numeroPersonas; i++) {
-    let edadPersona = 1;/*parseInt(prompt("Por favor ingrese edad de la persona."));*/
+    let edadPersona = 15;/*parseInt(prompt("Por favor ingrese edad de la persona."));*/
     if (edadPersona >= 0 || edadPersona <= 12) {
         promNiño = personaNiño / numeroPersonas;
         personaNiño++;
     } else {
         if (edadPersona >= 13 || edadPersona <= 29) {
+            promJovenes = personaJovenes / numeroPersonas;
             personaJovenes++;
         } else {
+
             if (edadPersona >= 30 || edadPersona <= 59) {
+                promAdultos = personaAdultos / numeroPersonas;
                 personaAdultos++;
             } else {
                 if (edadPersona >= 60) {
+                    promViejos = personaViejos / numeroPersonas;
                     personaViejos++;
+                } else {
                 }
             }
         }
     }
 }
 /*alert*/console.log("El promedio de niños es " + promNiño + " con una cantidad de " + personaNiño + " personas");
+/*alert*/console.log("El promedio de jovenes es " + promJovenes + " con una cantidad de " + personaJovenes + " personas");
+/*alert*/console.log("El promedio de adultos es " + promAdultos + " con una cantidad de " + personaAdultos + " personas");
+/*alert*/console.log("El promedio de viejos es " + promViejos + " con una cantidad de " + personaViejos + " personas");
