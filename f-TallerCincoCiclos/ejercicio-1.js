908a2c02d851f50768afc4ad6d3412bb5f30da0f
Jefphunter <jefphunter@gmail.com>
2023-03-17 17:18:51 -0500

    Ejercicio dos taller cinco ciclos

--- f-TallerCincoCiclos/ejercicio-1.js
+++ f-TallerCincoCiclos/ejercicio-1.js
@@ -1,3 +1,10 @@
 /*
 Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número.
 */
+let enteroPositivo = 10;/*parseInt(prompt("Por favor ingrese numero entero."));*/
+
+for (i = 0; i < enteroPositivo; i++) {
+    if (i % 2 == 1) {
+        console.log(i)
+    }
+}
