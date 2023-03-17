ec4c7a941198ce749db8ce1b18d4f79e1d76b078
Nicolasriaño <mnegocios339@gmail.com>
2023-03-17 14:29:36 -0500

    jaja

--- Taller ciclo/Punto1.js
+++ Taller ciclo/Punto1.js
@@ -1,29 +1,13 @@
-// Solicitamos al usuario que ingrese un número
-let numero = prompt("Ingrese un número");
-
-// Convertimos el número a una cadena de texto
-let numeroStr = numero.toString();
-
-// Inicializamos los índices del comienzo y fin de la cadena
-let i = 0;
-let j = numeroStr.length - 1;
-
-// Creamos un bucle while que se ejecuta mientras los índices no se cruzan
-while (i < j) {
-  // Si los caracteres en los índices correspondientes son diferentes,
-  // el número no es un palíndromo
-  if (numeroStr.charAt(i) !== numeroStr.charAt(j)) {
-    console.log("El número ingresado no es un palíndromo");
-    break;
-  }
-
-  // Incrementamos el índice de inicio y decrementamos el de fin
-  i++;
-  j--;
+let numero = parseInt(prompt("Ingrese un número"));
+let invertido = 0;
+let numTrabajo = numero;
+while (numTrabajo > 0) {
+  let digito = numTrabajo % 10;
+  invertido = (invertido * 10) + digito;
+  numTrabajo = Math.floor(numTrabajo / 10);
 }
-
-// Si el bucle while ha terminado y los índices se han cruzado, entonces
-// el número es un palíndromo
-if (i >= j) {
+if (numero === invertido) {
   console.log("El número ingresado es un palíndromo");
+} else {
+  console.log("El número ingresado no es un palíndromo");
 }
--- Taller ciclo/miejercicio.js
+++ Taller ciclo/miejercicio.js
@@ -1,12 +1,8 @@
-// Solicitamos la cantidad a invertir, tasa de interés anual y el número de años
+
 let capitalInicial = parseFloat(prompt("Ingrese la cantidad a invertir:"));
 let tasaInteres = parseFloat(prompt("Ingrese la tasa de interés anual (en porcentaje):"));
 let aniosInversion = parseInt(prompt("Ingrese el número de años de la inversión:"));
-
-// Convertimos la tasa de interés a su equivalente decimal
 tasaInteres /= 100;
-
-// Calculamos el capital obtenido en la inversión cada año utilizando un bucle while
 let capitalActual = capitalInicial;
 let anioActual = 1;
 
