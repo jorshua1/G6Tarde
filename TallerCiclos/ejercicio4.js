1da31a30ff6e9c956b5ec776c945c45e293e3ded
AnyGomez <anymelisa613@gmail.com>
2023-03-22 14:14:39 -0500

    actualizacion ejercicios

--- TallerCiclos/ejercicio4.js
+++ TallerCiclos/ejercicio4.js
@@ -6,7 +6,7 @@
 
 
 for(let i=1;i<=5;i++){
-    for (let j=1;i<=4;i++){
-        console.log(`1.${i}.${j} -`);
+    for (let j=1;j<=4;j++){
+        console.log(`1.${i}.${j} - 1.${i}.${j} - 1.${i}.${j}`);
     }
 }
