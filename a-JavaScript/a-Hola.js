dc72c8a4e15aab0a8ddc9db4e483d2dd297bf11b
Jefphunter <jefphunter@gmail.com>
2023-03-26 18:32:59 -0500

    Tipos de comentarios y error de Syntaxis

--- a-JavaScript/a-Hola.js
+++ a-JavaScript/a-Hola.js
@@ -1,3 +1,7 @@
-/*
-este es un comentario nuevo de restauracion
-*/
+// un comentario de una línea
+
+/* este es un comentario
+   más largo, de varias líneas
+ */
+
+/* Sin embargo, no se pueden /* anidar comentarios */ SyntaxError * /
