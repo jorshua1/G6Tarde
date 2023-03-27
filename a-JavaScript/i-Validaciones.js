25a6c920d0ff870a308eaac2ba220d9e51c18cc7
Jefphunter <jefphunter@gmail.com>
2023-03-27 15:21:43 -0500

    Cómo declarar variables.

--- a-JavaScript/i-Validaciones.js
+++ a-JavaScript/i-Validaciones.js
@@ -1,11 +1,15 @@
-nombre= "Fabian"/*prompt("Por favor ingrese nombre");*/
-while(!isNaN(nombre)){
-  console.log("Error");
-  nombre=prompt("Por favor ingrese nomnre");
+nombre = "Fabian"/*prompt("Por favor ingrese nombre");*/
+while (!isNaN(nombre)) {
+    console.log("Error");
+    nombre = prompt("Por favor ingrese nomnre");
 }
 console.log(nombre);
 
-nombreX="Fabian"/*prompt("Por favor ingrese nombre");*/
-for(nombreX;!isNaN(nombreX););{
-  nombreX="Fabian"/*prompt("Por favor ingrese nombre");*/
+nombreX = "Fabian"/*prompt("Por favor ingrese nombre");*/
+for (nombreX; !isNaN(nombreX);); {
+    nombreX = "Fabian"/*prompt("Por favor ingrese nombre");*/
 }
+
+
+let comparacionNumeros = (1 > 2) ? "si" : "No";
+console.log(comparacionNumeros);
