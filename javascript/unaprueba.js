6f263e966a3eb1da84ede890f23bbd3d955ccf1e
CarlosVillamizar <carvilpat@gmail.com>
2023-03-28 17:27:09 -0500

    La recuperacion que no debia hacer

---
+++ javascript/unaprueba.js
@@ -0,0 +1,9 @@
+let array=[]
+console.log(array.length)
+array.push("sexo")
+console.log(array.length)
+array.push("xdxd");
+console.log(array)
+let maracachafa = "mariwanita"
+array.push(maracachafa)
+console.log(array)
