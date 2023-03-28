1ec10bcc0e2d03b34ebe10e54768ce8eb680d714
Jefphunter <jefphunter@gmail.com>
2023-03-27 21:27:22 -0500

    Condicionales, If-Else y  tipos de usos.

--- a-JavaScript/c-If_Else__Else_If.js
+++ a-JavaScript/c-If_Else__Else_If.js
@@ -22,6 +22,22 @@ if (compraRealizada === true) {
     console.log(subsidioAdicional);
 }
 
+let edadUno = 13;/*parseInt(prompt("Ingrese su edad: "));*/
+
+if (edadUno >= 18) {
+    console.log("Es mayor de edad.");
+} else {
+    console.log("Es menor de edad.");
+}
+
+let edadDos = 20 /*parseInt(prompt("Ingrese su edad: "));*/
+
+if (edadDos >= 18) {
+    console.log("Es mayor de edad.");
+} else {
+    console.log("Es menor de edad.");
+}
+
 /*
 Notamos que no tenemos que incluir else y el segundo bloque de llaves — La siguiente declaración también es perfectmaente legal.
 Sin embargo, hay que ser cuidadosos — en este caso, el segundo bloque no es controlado por una declaración condicional, 
@@ -101,6 +117,18 @@ if (condicionUno === 10) {
 } else {
 }
 
+let edadCuatro = 35 /*parseInt(prompt("Ingrese su edad."));*/
+
+if (isNaN(edadCuatro)) {
+    console.log("Error");
+} else if (edadCuatro >= 1 && edadCuatro >= 18) {
+    console.log("Es mayor de edad.");
+} else if (edadCuatro >= 1 && edadCuatro <= 17) {
+    console.log("Es menor de edad");
+} else {
+    console.log("Ingrese una edad valida");
+}
+
 /*
 Podemis implementar una estructura similar al Switch pero empleando If y else.
 Hay momentos en JavaScript en los que podrías considerar usar una sentencia switch en lugar de una sentencia if else.
@@ -140,6 +168,16 @@ if (edad >= 16 && tieneCarro) {
     console.log("Fabian es lo suficiente mayor para conducir y tiene su propio carro.");
 } else {
     console.log("Fabian no conduce.");
+}
+
+let edadTres = 35 /*parseInt(prompt("Ingrese su edad."));*/
+
+if (edadTres > 0 && edadTres >= 18) {
+    console.log("Es mayor de edad");
+} else if (edadTres <= 0) {
+    console.log("Error, ingrese valor valido");
+} else {
+    console.log("Es menor de edad");
 }
 
 /*
@@ -191,7 +229,7 @@ const edadMia = 32;
 const ciudadano = edadMia >= 18 ? "Puede votar" : "No puede votar";
 console.log(ciudadano);
 
-//Comaprando con  if-else seria:
+//Comparando con  if-else seria:
 
 const edadSuya = 32;
 let ciudadanoDos;
@@ -216,81 +254,43 @@ El uso de operadores lógicos para probar varias condiciones puede reemplazar se
 
 El operador ternario se puede usar para escribir código más corto para una sentencia if else simple.
 */
-let edad1 =13;/*parseInt(prompt("Ingrese su edad: "));*/
+let edadCinco = 50 /*parseInt(prompt("Ingrese su edad."));*/
 
-if (edad1 >= 18) {
-    alert("Es mayor de edad.");
-    /*Codigo a ejecutar si es verdadero*/
+if (isNaN(edadCinco) || edadCinco < 0) {
+    console.log("ingrese valor valido");
+} else if (edadCinco > 0 && edadCinco >= 18) {
+    console.log("Es mayor de edad.");
 } else {
-    alert("Es menor de edad.");
-    /*Codigo a ejecutar si es falsa*/
+    console.log("Es menor de edad.");
 }
 
-let edad2 = parseInt(prompt("Ingrese su edad: "));
-
-if (edad2 >= 18) {
-    alert("Es mayor de edad.");
-} else {
-    alert("Es menor de edad.");
-}
-
-let edad3 = parseInt(prompt("Ingrese su edad."));
-
-if (edad3 > 0 && edad3 >= 18) {
-    alert("Es mayor de edad");
-} else if (edad3 <= 0) {
-    alert("Error, ingrese valor valido");
-} else {
-    alert("Es menor de edad");
-}
-
-let edad4 = parseInt(prompt("Ingrese su edad."));
-
-if (isNaN(edad4)) {
-    alert("Error");
-} else if (edad4 >= 1 && edad4 >= 18) {
-    alert("Es mayor de edad.");
-} else if (edad4 >= 1 && edad4 <= 17) {
-    alert("Es menor de edad");
-} else {
-    alert("Ingrese una edad valida");
-}
-
-let edad5 = parseInt(prompt("Ingrese su edad."));
-
-if (isNaN(edad5) || edad5 < 0) {
-    alert("ingrese valor valido");
-} else if (edad5 > 0 && edad5 >= 18) {
-    alert("Es mayor de edad.");
-} else {
-    alert("Es menor de edad.");
-}
-
-let numero = parseInt(prompt("Ingrese su numero."));
+let numero = 13;/*parseInt(prompt("Ingrese su numero."));*/
 
 if (isNaN(numero) || numero % 2 == 0) {
-    alert("Es numero par");
+    console.log("Es numero par");
 } else {
-    alert("Es numero impar.");
+    console.log("Es numero impar.");
 }
+
+
 /*control de temperaturas de galpones de engorde*/
 
-let temperatura = parseInt(prompt("Ingrese su lectura."));
+let temperatura = 32/*parseInt(prompt("Ingrese su lectura."));*/
 
 if (isNaN(temperatura) || temperatura >= 32) {
-    alert("Apagar calefactor del galpon.");
+    console.log("Apagar calefactor del galpon.");
 } else if (temperatura <= 27) {
-    alert("Encender calefactor del galpon.");
+    console.log("Encender calefactor del galpon.");
 } else {
-    alert("Temperatura estable, tomar registro de termometros fisicos");
+    console.log("Temperatura estable, tomar registro de termometros fisicos");
 }
 
-let diametroRueda = parseFloat(prompt("Ingrese su diametro."));
+let diametroRueda = 2;/*parseFloat(prompt("Ingrese su diametro."));*/
 
 if (isNaN(diametroRueda) || diametroRueda > 1.4) {
-    alert("La rueda es para vehiculo grande.");
+    console.log("La rueda es para vehiculo grande.");
 } else if (diametroRueda > 0.8 && diametroRueda <= 1.4) {
-    alert("Larueda es para vehiculo mediano.");
+    console.log("Larueda es para vehiculo mediano.");
 } else {
-    alert("La rueda es para vehiculo pequeño.");
+    console.log("La rueda es para vehiculo pequeño.");
 }
