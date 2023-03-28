1b3800d7e652586390602050f77d0c8b6efbd942
MACHADOSTEER <arturomachadosteer@gmail.com>
2023-03-28 18:16:16 -0500

    mision 2, no terminada

--- Algoritmos_ejemplos/ejemplo5.js
+++ Algoritmos_ejemplos/ejemplo5.js
@@ -13,3 +13,34 @@ if (genero == 'Masculino') {
 }
 
 //luis////
+opcionSubmenu=0
+datosNombre = prompt(`
+ingrese su nombre
+`)
+while (!isNaN(datosNombre) ) {
+  datosNombre = prompt(`
+      valor invalido
+  ingrese su nombre
+  `)
+
+
+
+  Altura = parseInt(prompt('ingrese su altura en metros'))
+
+  while (isNaN(Altura) || Altura <= 0) {
+    Altura = prompt(`
+        valor invalido
+    ingrese su altura en metros
+    `)
+
+    Peso = parseInt(prompt('ingrese su peso en kilogramos'))
+    while (isNaN(Peso) || Peso <= 0) {
+      Peso = prompt(`
+            valor invalido
+        ingrese su peso en kilogramos
+        `)
+
+      
+    }
+  }
+}
