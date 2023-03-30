88f5ff5159db09b3e65a79d4fd7e87194f46bab6
FelipeGil <xelpopez@gmail.com>
2023-03-30 15:31:00 -0500

    30 mar

---
+++ arreglos/tablas.js
@@ -0,0 +1,14 @@
+let tabla =  [];
+let tabla2 = [];
+
+for(fila = 0; fila < 4; fila++){
+    console.log(`Creando fila ${fila + 1 }`);
+    tabla [fila] = [];
+    for(columna = 0; columna < 2; columna++){
+        console.log(`Está insertando datos en la fila ${fila + 1} y columna ${columna + 1}`);
+
+        tabla [fila][columna] = prompt("INGRESE ALGO");
+    }
+    console.log(`Se ingresaron los datos de la fila ${fila + 1} y columna ${columna + 1}`);
+}
+console.log(tabla);
