f38b663f05245d697fb030d8f0cb1978d84dc90b
Jay <danielpinzon20jay@gmail.com>
2023-03-30 15:16:03 -0500

    se agrego matrices

--- ARRAYS/matrices.js
+++ ARRAYS/matrices.js
@@ -1,2 +1,11 @@
 //las matrices son arreglos de dos divisiones
-let array=[3]
+let tabla = [];
+for(let fila=0;fila<4;fila++){
+    console.log(`creando fila ${fila + 1}`)
+    tabla[fila]=[]
+    for(let columna=0;columna<2;columna++){
+        console.log(`Esta insertando datos en la fila ${fila + 1} y la columna ${columna+1}`)
+        tabla[fila][columna] = prompt("Ingrese dato")
+    }
+}
+console.log(tabla)
