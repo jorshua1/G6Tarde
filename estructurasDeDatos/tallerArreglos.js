cbd0686bdc8795aed07226516f08058a9bd01952
MACHADOSTEER <arturomachadosteer@gmail.com>
2023-04-10 17:45:55 -0500

    ordenados

--- estructurasDeDatos/tallerArreglos.js
+++ estructurasDeDatos/tallerArreglos.js
@@ -1,21 +1,9 @@
 let numeros= [10]
-let suma=[1]
 for(let i=0;i<10;i++){
-dato=Number(prompt("ingrese un valor numerico"))
-numeros[i]=dato
+
+numeros[i]=Number(prompt("ingrese un valor numerico"))
+
+}
+
 console.log(numeros)
-
-}
-
-for(let i=0;i<1;i++){
-
-suma[i]=numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+numeros[7]+numeros[8]+numeros[9]+numeros[10]
-
-
-
-}
-
-console.log(suma)
-
-
 
