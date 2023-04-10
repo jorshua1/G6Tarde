6467c46db202795cb9b349d9a0e4cfa73d012e47
brayanSantamaria <brayan.santamaria.2002@gmail.com>
2023-04-10 17:21:23 -0500

    taller con matrices

--- javascript/talleres/punto1.js
+++ javascript/talleres/punto1.js
@@ -9,13 +9,13 @@ let suma=0;
             unmenudepurovisaje=Number(prompt("bienvenido a el algoritmo de suma\n en este algoritmo podra sumar 10 numeros que se ingresen por teclado\n selecione una de las siguientes opciones para continuar:\n 1. iniciar la suma\n 2. salir del programa"));
     }
     if(unmenudepurovisaje==1){
-for(let i=0; i<10; i++){
+for(let i=0; i<11; i++){
     if(unmenudepurovisaje==1){
         numerosingresados=Number(prompt("ingrese los datos que desea empezar a sumar"));
     }
     vector[i]=numerosingresados;
 }
-for(let i=0;i<10;i++){
+for(let i=0;i<11;i++){
     suma+=vector[i];
 }
 alert("la suma total es igual a "+suma+"\n adios");
---
+++ javascript/talleres/tallermatrices/punto3.js
@@ -0,0 +1,8 @@
+let filas=[];
+let columnas=[];
+for(let a=0;a<3;a++){
+    for(let i=0;i<3;i++){
+        var columna=Number(prompt("esta ingresando numeros en la fila "+a+" columna"+i));
+        columnas[a][i]=columna;
+    }
+}
