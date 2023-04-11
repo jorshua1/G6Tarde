710dc8ad6e4336cceabd78891090feb5a4e7347c
Nicolas Ruiz <nicosandr22@gmail.com>
2023-04-11 17:44:26 -0500

    quicksort

--- javaScript/Estructura Datos/OrdenamientoDatos.js
+++ javaScript/Estructura Datos/OrdenamientoDatos.js
@@ -1,6 +1,8 @@
 /*Hacer un programa que inicialice un vector de numeros con valores aleatorios 
 y posteriormente ordene los elementos de menor a mayor*/
 
+//Ordemaniento de datos usando el metodo Burbuja ---------------------
+
 let numeroMaximo = 0;
 let vector = new Array();
 
@@ -15,8 +17,6 @@ for (let i = 0; i < numeroMaximo; i++) {
 }
 console.log(vector);
 
-//Ordemaniento de datos usando el metodo Burbuja 
-
 for (let i = 0; i < numeroMaximo; i++) {
     for (let j = 0; j < vector.length-1; j++) {
 
@@ -28,6 +28,88 @@ for (let i = 0; i < numeroMaximo; i++) {
 }
 console.log(vector);
 
-//Ordenamiento usando Quick sort
+//Ordenamiento usando Quick sort ---------------------------------------------
+
+let myArr = [];
+
+maximo = parseInt(prompt(`Ingrese un numero maximo para generar un vector de numeros aleatorios.`));
+while(isNaN(maximo) || maximo <= 0){
+    maximo = parseInt(prompt(`POR FAVOR ingrese un numero maximo para generar un vector de numeros aleatorios.`));
+}
+
+for (let i = 0; i < maximo; i++) {
+    myArr[i] = Math.floor(Math.random() * (100 - (-100))) + (-100);
+
+}
+console.log(myArr);
+ 
+const quickSort = (arr) => {    
+    
+    if(arr.length === 1) {
+        return arr;
+    }
+
+    let pivot = arr[arr.length - 1];
+    let leftArray = [];
+    let rightArray = [];
+    for(let i = 0; i < arr.length - 1; i++) {
+       if(arr[i] <= pivot) {
+           leftArray.push(arr[i]);
+       } else {
+           rightArray.push(arr[i]);
+       }
+    }
+    if(leftArray.length > 0 && rightArray.length > 0) {
+        return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+    } else if (leftArray.length > 0) {
+        return [...quickSort(leftArray), pivot]
+    } else {
+        return [pivot, ...quickSort(rightArray)]
+    }
+}
+console.log(quickSort(myArr))
 
 
+
+//--------------------------
+/**
+ 
+let myArr = [];
+
+maximo = parseInt(prompt(`Ingrese un numero maximo para generar un vector de numeros aleatorios.`));
+while(isNaN(maximo) || maximo <= 0){
+    maximo = parseInt(prompt(`POR FAVOR ingrese un numero maximo para generar un vector de numeros aleatorios.`));
+}
+
+for (let i = 0; i < maximo; i++) {
+    myArr[i] = Math.floor(Math.random() * (100 - (-100))) + (-100);
+
+}
+console.log(myArr);
+ myArr = [1, 1, 2, 5, 0, -5];
+const quickSort = (arr) => {    
+    
+    if(arr.length === 1) {
+        return arr;
+    }
+
+    let pivot = arr[arr.length - 1];
+    let leftArray = [];
+    let rightArray = [];
+    for(let i = 0; i < arr.length - 1; i++) {
+       if(arr[i] <= pivot) {
+           leftArray.push(arr[i]);
+       } else {
+           rightArray.push(arr[i]);
+       }
+    }
+    if(leftArray.length > 0 && rightArray.length > 0) {
+        return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+    } else if (leftArray.length > 0) {
+        return [...quickSort(leftArray), pivot]
+    } else {
+        return [pivot, ...quickSort(rightArray)]
+    }
+}
+console.log(quickSort(myArr))
+ */
