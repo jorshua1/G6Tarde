6ef25433c65fcc9357f192b520620965eb6f2a54
Jefphunter <jefphunter@gmail.com>
2023-04-12 16:45:22 -0500

    ejercicio uno h-TallerSieteMatrices

--- h-TallerSieteMatrices/ejercicio-1.js
+++ h-TallerSieteMatrices/ejercicio-1.js
@@ -1,3 +1,27 @@
 /*Realizar un programa que defina un vector llamado “vector_numeros” de 10 enteros, a continuación lo inicialice con 
 valores aleatorios (del 1 al 10) y posteriormente muestre en pantalla cada elemento del vector junto con su cuadrado 
 y su cubo.*/
+
+let vectorNumeros = new Array(10);
+let vectorCuadrado = []
+let vectorCubico = []
+
+for (var i = 0; i < vectorNumeros.length; i++) {
+    let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+    vectorNumeros[i] = numeroAleatorio;
+    console.log(vectorNumeros);
+
+    for (let i = 0; i < vectorNumeros.length; i++) {
+        vectorCuadrado[i] = vectorNumeros[i] ** 2;
+        console.log(`el cuadrado de ${vectorNumeros[i]} es ${vectorCuadrado[i]}`);
+
+        for (let i = 0; i < vectorNumeros.length; i++) {
+            vectorCubico[i] = vectorNumeros[i] ** 3;
+            console.log(`el cubico de ${vectorNumeros[i]} es ${vectorCubico[i]}`);
+        }
+    }
+}
+console.log(`
+El vector inicial es:      ${vectorNumeros}
+El cuadrado del vector es: ${vectorCuadrado}
+El cubico del vector es:   ${vectorCubico}`)
