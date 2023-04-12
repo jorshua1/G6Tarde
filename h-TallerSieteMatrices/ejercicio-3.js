09e2011db38eedab0bb0f67d3b81e52a3c8a53a2
Jefphunter <jefphunter@gmail.com>
2023-04-12 17:43:19 -0500

    Desarranllo Ejercicio Tres h-TallerSieteMatrices

--- h-TallerSieteMatrices/ejercicio-3.js
+++ h-TallerSieteMatrices/ejercicio-3.js
@@ -1,2 +1,14 @@
 /*Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas entre 0 y 10). 
 A continuación debe mostrar todas las notas, la nota media, la nota más alta que ha sacado y la menor.*/
+
+let notaAlumno = 0;
+let vectorNotas = new Array(5)
+
+for (i = 0; i < vectorNotas.length; i++) {
+
+    if(isNaN(notaAlumno)){
+        console.log("Por favor ingrese un valor valido")
+    }
+    notaAlumno = parseInt(prompt("Por favor ingrese las notas del estudiante."));
+
+}
